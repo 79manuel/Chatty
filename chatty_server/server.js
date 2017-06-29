2463ff125d90abf9b1c38a748f6caadfb8bebb3a
Manuel <prezcabellomanuel@gmail.com>
2017-06-29 01:55:27 +0000

    Incoming messages displayed

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
+const WebSocket = require('ws');
+const uuidV4 = require('uuid/v4');
 
 // Set the port to 3001
 const PORT = 3001;
@@ -13,16 +15,30 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
+wss.broadcast = function broadcast(data) {
+  console.log("in broadcast",data);
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
-  ws.send('something');
 
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    var incomingMsg = message;
+    incomingMsg.id = uuidV4();
+    var outGoingMsg = incomingMsg;
+    console.log('ws message received: %s', JSON.parse(message).content);
+    wss.broadcast(outGoingMsg);
+
   });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,27 @@ import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 import Navbar from './NavBar.jsx';
 
-const users = {
-  currentUser: {name: "Test"},
-  messages: [
-    {
-      username: "Senor Fluffyface",
-      content: "Has anyone seen my marbles?",
-      id:"01"
-    },
-    {
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-      id:"02"
-    }
-  ]
-};
 
 class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = users;
+    this.state = {
+      currentUser: {name: "Mr Fluffyface"},
+      messages: []
+    };
   }
 
   makeNewMessage = (message) => {
-      console.log(message.message, message.username);
-      const newMessage = {id: Date.now(),
+      //console.log(message.message, message.username);
+      const newMessage = {
         username: message.username,
-        content: message.message};
-      const messages = this.state.messages.concat(newMessage)
-      this.setState({messages: messages})
+        content: message.message
+      };
+      //const messages = this.state.messages.concat(newMessage);
+      this.state.messages.concat(newMessage);
+      //this.setState({messages: messages})
+      this.broadcastMessage(JSON.stringify(newMessage))
   }
 
   broadcastMessage(message) {
@@ -40,10 +31,11 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.socket = new WebSocket('ws://localhost:3000/');
+    this.socket = new WebSocket('ws://localhost:3001/');
     this.socket.addEventListener('message', (event) => {
       const newMessage = JSON.parse(event.data);
       const newMessages = this.state.messages.concat(newMessage);
+      console.log("componentdidmountrohit".newMessages);
       this.setState({
         messages: newMessages
       });
